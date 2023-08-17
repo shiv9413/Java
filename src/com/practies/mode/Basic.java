23898793fbff59a3136b2b8234c8eb583f21b45c
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-08-17 10:24:05 +0530

    changes

--- src/com/practies/mode/Basic.java
+++ src/com/practies/mode/Basic.java
@@ -2,6 +2,6 @@ package com.practies.mode;
 
 public class Basic {
 	public static void main(String[] args) {
-		System.out.println("Hello This my first project");
+		System.out.println("This is working Fine Yaar");
 	}
 }
