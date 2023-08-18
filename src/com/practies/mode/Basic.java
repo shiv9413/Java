9a487527a2de1bff255a16399f379130f46b7e0f
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-08-18 10:15:49 +0530

    18aug

--- src/com/practies/mode/Basic.java
+++ src/com/practies/mode/Basic.java
@@ -1,7 +1,25 @@
 package com.practies.mode;
 
 public class Basic {
-	public static void main(String[] args) {
-		System.out.println("This is working Fine Yaar");
+	public static void main(String args[]) {
+		// println method will add new line at the end of output
+		System.out.println("Hello this is my second text");
+		System.out.println("Hello This is second line of code");
+		System.out.println("Hello This is third line of code");
+		System.out.println("Hello this is forth line of code");
+		
+		// Without add double quotes can print numbers
+		System.out.println(2);
+		System.out.println(2+2+2+2+2);
+		var a = 10; var b = 20;
+		System.out.println(a+b);
+		
+		//Print method will not add new line at the end of output
+		System.out.print("Hello this is fivth line of code");
+		System.out.print("Hello this is sixth line of code");
+		System.out.print("Hello this is sevth line of code");
+		System.out.print(3+3+3+3+3);
+		
+		/* Next Topic Start With Java Variables */
 	}
 }
