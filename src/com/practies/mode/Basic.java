6d9c67c20d1ee8766ba749115ea1a6d3d9c67434
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-08-21 15:39:12 +0530

    Changes

--- src/com/practies/mode/Basic.java
+++ src/com/practies/mode/Basic.java
@@ -3,10 +3,10 @@ package com.practies.mode;
 public class Basic {
 	public static void main(String args[]) {
 		// println method will add new line at the end of output
-		System.out.println("Hello this is my second text");
-		System.out.println("Hello This is second line of code");
-		System.out.println("Hello This is third line of code");
-		System.out.println("Hello this is forth line of code");
+		System.out.println("Hello this is our first line");
+		System.out.println("Hello this is our second line");
+		System.out.println("Hello this is our third line");
+		System.out.println("Hello this is our forth line");
 		
 		// Without add double quotes can print numbers
 		System.out.println(2);
@@ -18,9 +18,17 @@ public class Basic {
 		System.out.print("Hello this is fivth line of code");
 		System.out.print("Hello this is sixth line of code");
 		System.out.print("Hello this is sevth line of code");
-		System.out.print(3+3+3+3+3);
+		System.out.println(3+3+3+3+3);
 		
 		/* Next Topic Start With Java Variables */
+		
+		System.out.println("First we will discuss String Type Variables");
+		
+		String firstname = "Gagan";
+		String middlename = "Deep";
+		String lastname = "Kashyap";
+		
+		System.out.println("First Name is "+firstname+" Middle Name is "+middlename+" Last Name is "+lastname);
 		
 	}
 }
