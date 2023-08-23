849bd2f7c7b3aa530e13b2ce0daeda42594ae46c
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-08-23 11:02:31 +0530

    operatos

---
+++ src/com/operators/mode/JavaOperators.java
@@ -0,0 +1,10 @@
+package com.operators.mode;
+import java.util.Scanner;
+
+public class JavaOperators {
+	public static void main(String args[]) {
+		System.out.println("Which Operation you want to perform");
+		System.out.println("1 for Arthmetic operations");
+		System.out.println("2 for Assignment Operations");
+	}
+}
