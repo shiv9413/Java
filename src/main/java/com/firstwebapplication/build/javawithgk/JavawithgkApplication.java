e65a8f1817baf5335779d10ab19562f530b3992c
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-08-31 12:03:38 +0530

    Changes

---
+++ src/main/java/com/firstwebapplication/build/javawithgk/JavawithgkApplication.java
@@ -0,0 +1,13 @@
+package com.firstwebapplication.build.javawithgk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawithgkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawithgkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstwebapplication/build/javawithgk/JavawithgkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstwebapplication.build.javawithgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawithgkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
