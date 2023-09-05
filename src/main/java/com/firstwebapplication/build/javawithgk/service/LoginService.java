d9eea3a380690ad4158ea454ba9588e5b4fe2e9e
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-09-05 10:50:44 +0530

    Changes

---
+++ src/main/java/com/firstwebapplication/build/javawithgk/service/LoginService.java
@@ -0,0 +1,15 @@
+package com.firstwebapplication.build.javawithgk.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class LoginService {
+
+    public boolean validateUser(String userid, String password) {
+        // in28minutes, dummy
+        return userid.equalsIgnoreCase("in28minutes")
+                && password.equalsIgnoreCase("dummy");
+    }
+}
